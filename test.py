ac6dce3c1783a2b0a2d0e0b4b5a75e873301fd83
IvanaBeydoun <20510413@aou.edu.sa>
2024-09-17 12:18:13 +0300

    final code

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
+import logging
+
+
 print("Hello Word")
+print("world hello")
+logging.basicConfig(
+    filename='app.log',      # Log file name
+    level=logging.DEBUG,     # Log level
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+# Example usage
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.debug('This is a debug message')
+    logger.info('This is an info message')
+    logger.warning('This is a warning message')
+    logger.error('This is an error message')
+    logger.critical('This is a critical message')
+
+if __name__ == '__main__':
+    main()
